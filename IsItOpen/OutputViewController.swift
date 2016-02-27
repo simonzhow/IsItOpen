557571eb31921424caf8620fa802a7b4a5db83aa
Simon Zhou <simon.zhou260@gmail.com>
2016-02-27 00:35:40 -0800

    Two View Controllers

--- IsItOpen/OutputViewController.swift
+++ IsItOpen/OutputViewController.swift
@@ -6,5 +6,21 @@
 //  Copyright © 2016 Simon Zhou. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class OutputViewController: UIViewController{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- IsItOpen/ViewController.swift
+++ IsItOpen/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet var searchTextField: UITextField!
     
     
     
@@ -22,9 +23,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        searchTextField.resignFirstResponder()
+        return true
     }
 
 
