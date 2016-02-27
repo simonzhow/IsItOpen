2a0b35b2532d8e0515847c2893b5625b8bc485ef
bibekg <ghim.bibek@gmail.com>
2016-02-27 01:55:31 -0800

    table-view-boilerplate-added

--- IsItOpen/ViewController.swift
+++ IsItOpen/ViewController.swift
@@ -8,16 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate {
-
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var searchTextField: UITextField!
-    
     @IBOutlet var tableView: UITableView!
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +22,12 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         
         self.searchTextField.delegate = self
     }
-
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
     }
 
-    func textFieldShouldReturn(textField: UITextField) -> Bool
-    {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
         searchTextField.resignFirstResponder()
         return true
     }
@@ -48,6 +35,17 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+
+    
+    // Table View Functions
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
     }
 
 
